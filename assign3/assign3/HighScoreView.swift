f001162b82ba6b902e6ca16c055a19f89fb28098
Shahryar Shagoshtasbi <shahryar9782@gmail.com>
2021-04-13 18:42:18 -0400

    High score semi-Functional

--- assign3/assign3/HighScoreView.swift
+++ assign3/assign3/HighScoreView.swift
@@ -8,26 +8,28 @@
 import SwiftUI
 
 struct HighScoreView: View {
+    @StateObject var scoreList = ScoreList()
     var body: some View {
         NavigationView{
-            List{
-                Text("Hi")
-//                                Score(score: 5, time: Date.init());
-            }.navigationTitle("High Scores")
-            
-        }
-        //        Text("Hello From High Score View")
+            List(0..<scoreList.scoreList.count, id: \.self) { score in
+                Text("score is  \(scoreList.scoreList[score])" as String)
+            }
+        }.navigationTitle("High Scores")
+        
     }
+    //        Text("Hello From High Score View")
 }
 
 
-struct Score: Hashable {
-//    static func == (lhs: Score, rhs: Score) -> Bool {
-//        lhs.score == rhs.score
-//    }
+
+struct Score: Hashable, Equatable {
+    //    static func == (lhs: Score, rhs: Score) -> Bool {
+    //        lhs.score == rhs.score
+    //    }
     
     var score: Int
     var time: Date
+//    var id: UUID
     
     func hash(into hasher: inout Hasher) {
         hasher.combine(time)
@@ -36,14 +38,19 @@ struct Score: Hashable {
     init(score: Int, time: Date) {
         self.score = score
         self.time = time
+//        self.id = UUID()
     }
 }
 
-class ScoreList {
-    @Published var scoreList = 0
+class ScoreList: Identifiable, ObservableObject{
+    @Published var scoreList = [Score]()
     
     init() {
         scoreList = [Score (score: 300, time: Date.init()), Score(score: 400, time: Date.init())]
+    }
+    func addNewScore(score: Score){
+        scoreList.append(score)
+        self.scoreList = scoreList.sorted{$0.score > $1.score}
     }
 }
 
