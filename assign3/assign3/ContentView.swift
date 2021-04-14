ce7fb57ad91c55ef272057b863cb715fc2853536
Shahryar Shagoshtasbi <shahryar9782@gmail.com>
2021-04-14 15:01:40 -0400

    Landscape mode semi good

--- assign3/assign3/ContentView.swift
+++ assign3/assign3/ContentView.swift
@@ -12,45 +12,48 @@ struct ContentView: View {
     @EnvironmentObject var scoreList: ScoreList
     @StateObject var logicObj = Triples()
     @State var rand: Bool = true
-//        .environmentObject(scoreList)
+    //        .environmentObject(scoreList)
+    
+    @Environment(\.verticalSizeClass) var verticalSizeClass
     
     var body: some View {
         ZStack{
             Color(red:250 / 255 , green: 200 / 255 , blue:135 / 255)
                 .ignoresSafeArea()
-            VStack{
-//                Spacer()
-                Score_Label().environmentObject(logicObj)
-                Spacer().scaledToFit()
-                
-                MainRec()
-                    .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local).onEnded(({value in
-                        if value.translation.width > 0 {
-                            logicObj.collapse(dir: .right)
-                            logicObj.spawn()
-                        }else if value.translation.width < 0 {
-                            logicObj.collapse(dir: .left)
-                            logicObj.spawn()
-                        }else if value.translation.height > 0 {
-                            logicObj.collapse(dir: .down)
-                            logicObj.spawn()
-                        }else if value.translation.height < 0 {
-                            logicObj.collapse(dir: .up)
-                            logicObj.spawn()
-                        }
-                    })))
-                    .environmentObject(logicObj)
-                
-                Spacer()
-                    .scaledToFit()
-                
-                BtnArea(rand:$rand).environmentObject(logicObj)
-                Spacer()
-                    .scaledToFit()
-                RandOrDeter(rand: $rand).environmentObject(logicObj)
-                
-            }.alert(isPresented:
-                        $logicObj.gameOver){
+            if verticalSizeClass == .regular{
+                VStack{
+                    //                Spacer()
+                    Score_Label().environmentObject(logicObj)
+                    Spacer().scaledToFit()
+                    
+                    MainRec()
+                        .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local).onEnded(({value in
+                            if value.translation.width > 0 {
+                                logicObj.collapse(dir: .right)
+                                logicObj.spawn()
+                            }else if value.translation.width < 0 {
+                                logicObj.collapse(dir: .left)
+                                logicObj.spawn()
+                            }else if value.translation.height > 0 {
+                                logicObj.collapse(dir: .down)
+                                logicObj.spawn()
+                            }else if value.translation.height < 0 {
+                                logicObj.collapse(dir: .up)
+                                logicObj.spawn()
+                            }
+                        })))
+                        .environmentObject(logicObj)
+                    
+                    Spacer()
+                        .scaledToFit()
+                    
+                    BtnArea(rand:$rand).environmentObject(logicObj)
+                    Spacer()
+                        .scaledToFit()
+                    RandOrDeter(rand: $rand).environmentObject(logicObj)
+                    
+                }.alert(isPresented:
+                            $logicObj.gameOver){
                     Alert(title: Text("Game Over!"),
                           message: Text("Score: \(logicObj.score)"),
                           dismissButton: Alert.Button.default(
@@ -63,17 +66,70 @@ struct ContentView: View {
                           )
                     )
                 }
-            
+            }else { // Horizantal View
+                HStack{
+                    VStack{
+                        
+//                        Spacer().scaledToFit()
+                        
+                        MainRec()
+                            .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local).onEnded(({value in
+                                if value.translation.width > 0 {
+                                    logicObj.collapse(dir: .right)
+                                    logicObj.spawn()
+                                }else if value.translation.width < 0 {
+                                    logicObj.collapse(dir: .left)
+                                    logicObj.spawn()
+                                }else if value.translation.height > 0 {
+                                    logicObj.collapse(dir: .down)
+                                    logicObj.spawn()
+                                }else if value.translation.height < 0 {
+                                    logicObj.collapse(dir: .up)
+                                    logicObj.spawn()
+                                }
+                            })))
+                            .environmentObject(logicObj)
+                        
+                        Spacer()
+                            .scaledToFit()
+                    }
+                    Spacer()
+                    VStack{
+                        Spacer().scaledToFit()
+                        BtnArea(rand:$rand).environmentObject(logicObj)
+                        Spacer().scaledToFit()
+                           
+
+//                        Spacer()
+                    }
+                    VStack{
+                        Score_Label().environmentObject(logicObj)
+                        RandOrDeter(rand: $rand).environmentObject(logicObj)
+                    }
+                   
+                    
+                }.alert(isPresented:
+                            $logicObj.gameOver){
+                    Alert(title: Text("Game Over!"),
+                          message: Text("Score: \(logicObj.score)"),
+                          dismissButton: Alert.Button.default(
+                            Text("Start New Game"),
+                            action: {
+                                let newScore = Score(score: logicObj.score, time: Date())
+                                scoreList.addNewScore(score: newScore)
+                                logicObj.newgame(rand: rand)
+                            }
+                          )
+                    )
+                }
+            }
         }.onAppear{
             logicObj.newgame(rand: true)
-            //        }.alert(isPresented: logicObj.gameOver){
-            //            Alert(title: Text("Game Over"), message: Text("Your Score is \(logicObj.score)"), primaryButton: .destructive(Text("New Game"), action:{ logicObj.newgame(rand: false)}), secondaryButton: .cancel(Text("boo"), action: {}))
-            //        }
         }
     }
     
-  
-
+    
+    
     
 }
 
